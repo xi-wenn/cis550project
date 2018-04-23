2b77231a961f3e6a6921d24ecb4239ffebc2021e
Jacob Cui <jacobcui@seas.upenn.edu>
2018-04-23 19:55:15 -0400

    add login/register support for nosql

--- src/index.js
+++ src/index.js
@@ -1,61 +1,102 @@
-var express = require('express')
-var path = require('path')
-var app = express()
+var express = require('express');
+var path = require('path');
+var app = express();
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 
+var configDB = require('./config/database.js');
 
+// configuration ===============================================================
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+// set up our express application
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser.json()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({
+    secret: 'ilovescotchscotchyscotchscotch', // session secret
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+// login routes ======================================================================
+require('./app/routes.js')(app, passport);
+
+// mysql onfiguration ===============================================================
 var mysql = require('mysql');
-var connection = mysql.createConnection({
+var sql_connection = mysql.createConnection({
     host: 'mysql550project.cl1yl0dhh5zw.us-east-1.rds.amazonaws.com',
     user: 'liuliuliu',
     password: 'sixsixsix',
     database: 'myFlights'
 });
 
-connection.connect(function(err) {
+sql_connection.connect(function(err) {
     if (err) {
         console.error('error connecting: ' + err.stack);
         return;
     }
 
-    console.log('connected as id ' + connection.threadId);
+    console.log('connected as id ' + sql_connection.threadId);
 });
 
-//-------------mongodb---------------
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://liuliuliu:sixsixsix@54.147.46.161:27017/cool_db', function (err, db) {
-    if (err) {
-        throw err;
-    } else {
-        console.log("successfully connected to the database");
-    }
-    //db.close();
-});
+// //-------------mongodb---------------
+// var Schema = mongoose.Schema;
+// mongoose.connect('mongodb://liuliuliu:sixsixsix@54.147.46.161:27017/cool_db', function(err, db) {
+//     if (err) {
+//         throw err;
+//     } else {
+//         console.log("successfully connected to the database");
+//     }
+//     //db.close();
+// });
+
+
 
 //-------------connection test--------------
-/* // Create a schema
-var TodoSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
-  note: String,
-  updated_at: { type: Date, default: Date.now },
-});
-// Create a model based on the schema
-var Todo = mongoose.model('Todo', TodoSchema);
-// Create a todo in memory
-var todo = new Todo({name: 'Master NodeJS', completed: false, note: 'Getting there...'});
-// Save it to database
-todo.save(function(err){
-  if(err)
-    console.log(err);
-  else
-    console.log(todo);
-}); */
+// Create a schema
+// var TodoSchema = new mongoose.Schema({
+//     name: String,
+//     completed: Boolean,
+//     note: String,
+//     updated_at: { type: Date, default: Date.now },
+// });
+// // Create a model based on the schema
+// var Todo = mongoose.model('Todo', TodoSchema);
+// // Create a todo in memory
+// var todo = new Todo({ name: 'Master NodeJS', completed: false, note: 'Getting there...' });
+// // Save it to database
+// todo.save(function(err) {
+//     if (err)
+//         console.log(err);
+//     else
+//         console.log(todo);
+// });
 //---------mongodb---------------
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 var bodyParser = require('body-parser')
 var jsonParser = bodyParser.json()
+
+app.get('/', function(request, response) {
+    response.sendFile(path.join(__dirname, '/', 'index.html'));
+})
 
 app.get('/index', function(request, response) {
     response.sendFile(path.join(__dirname, '/', 'index.html'));
@@ -65,9 +106,9 @@ app.get('/search', function(request, response) {
     response.sendFile(path.join(__dirname, '/', 'search.html'));
 })
 
-app.get('/login', function(request, response) {
-    response.sendFile(path.join(__dirname, '/', 'login.html'));
-})
+// app.get('/login', function(request, response) {
+//     response.sendFile(path.join(__dirname, '/', 'login.html'));
+// })
 
 app.get('/airlineData', function(request, response) {
     // console.log("get airline data");
